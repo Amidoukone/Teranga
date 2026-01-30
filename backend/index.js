8b7288f04fd2f417e382c26fccf68409cbddb1c1
Amidoukone <142462945+Amidoukone@users.noreply.github.com>
2026-01-30 22:43:13 +0000

    Harden security headers and cookie auth mode

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const path = require('path');
 require('dotenv').config();
 const logger = require('./src/utils/logger');
 const requestContext = require('./src/middleware/requestContext.middleware');
+const securityHeaders = require('./src/middleware/securityHeaders.middleware');
 
 // Sequelize (models/index.js)
 const db = require('./models');
@@ -37,6 +38,7 @@ process.on('uncaughtException', (err) => {
    ====================================================== */
 app.set('trust proxy', 1);
 app.use(requestContext);
+app.use(securityHeaders);
 app.use(cookieParser());
 
 function normalizeOrigin(value) {
