d19b2dcec535f7934a4a5468e5d3c8e87b832aad
Amidoukone <142462945+Amidoukone@users.noreply.github.com>
2026-02-02 19:42:59 +0000

    Fix dotenv loading for backend

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,4 +1,8 @@
-require('dotenv').config();
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
 
 module.exports = {
   development: {
